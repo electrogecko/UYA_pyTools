9d929048c84d090633094b3c601020a5b0e7b167
electrogecko <a@max.re>
2025-09-08 12:46:07 -0700

    Create tank.c

---
+++ moby/tank.c
@@ -0,0 +1,6 @@
+// Health livemem in korg
+// 20 42 00 00 20 42
+
+// Speed is likely in a physics table
+// Lost the value, but its |float| < 1
+// might be one of these 20333F40 203340F4 20334190 21CB5AD0 21CB59D8
